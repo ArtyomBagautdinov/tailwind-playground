244e2813b420f01bec5f6f051ef75da5703326bc
ArtyomBagautdinov <bagautdinovartyom@gmail.com>
2021-03-14 09:17:56 +0200

    continue

--- components/poolTypes/poolTypes.vue
+++ components/poolTypes/poolTypes.vue
@@ -3,8 +3,8 @@
     <div class="container mx-auto px-4 py-6">
       <p class="text-gray-800 text-center xl:text-3xl lg:text-2xl md:text-large sm:text-base text-sm ">мы предлагаем вам</p>
       <h2 class="text-gray-900 leading-tight text-center font-bold xl:text-5xl lg:text-4xl md:text-2xl sm:text-xl text-xl">Виды возводимых бассейнов</h2>
-      <div class="grid xl:grid-cols-3 xl:gap-4 lg:grid-cols-3 lg:gap-4 md:grid-cols-1 md:gap-0 sm:grid-cols-1 sm:gap-0 grid-cols-1 gap-0 py-6">
-          <pool-types-item v-for="val in poolTypesItems" :key="val.id" :imageUrl="val.imageUrl" :title="val.title" :paragraph="val.paragraph" />
+      <div class="grid xl:grid-cols-3 xl:gap-5 lg:grid-cols-3 lg:gap-4 md:grid-cols-1 md:gap-2 sm:grid-cols-1 sm:gap-2 grid-cols-1 gap-2 py-6">
+          <pool-types-item v-for="val in poolTypesItems" :key="val.id" :imageUrl="val.imageUrl" :title="val.title" :paragraph="val.paragraph" :listItems="val.listItems" />
       </div>
     </div>
   </section>
@@ -24,19 +24,36 @@ export default {
                     id: 1,
                     title: "композитный бассейн",
                     paragraph: "Композитные бассейны пользуются всё большей популярностью у потребителей. И неспроста, ведь искусственные водоёмы из композита обладают рядом значительных преимуществ:",
-                    imageUrl : require('@/assets/images/compositePool.png')
+                    imageUrl : require('@/assets/images/compositePool.png'),
+                    listItems: [
+                        'Простота в монтаже и демонтаже',
+                        'Неприхотливость в уходе',
+                        'Легкость в обслуживании',
+                        'Ремонтопригодность'
+                    ]
                 },
                 {
                     id: 2,
                     title: "полипропиленовый бассейн",
                     paragraph: "Бассейны из полипропилена в несколько раз дешевле бетонных, материал устойчив к воздействию высоких и низких температур. Основные преимущества бассейнов из полипропилена:",
-                    imageUrl : require('@/assets/images/polypropylenePool.png')
+                    imageUrl : require('@/assets/images/polypropylenePool.png'),
+                    listItems: [
+                        'Устойчивость к повреждениям',
+                        'Устойчивость к температурам',
+                        'Разнообразие форм и размеров',
+                        'Не самая высокая цена'
+                    ]
                 },
                 {
                     id: 3,
                     title: "бетонный бассейн",
                     paragraph: "Бетонный бассейн относится к наиболее сложным и затратным конструкциям, однако отличаются высокой долговечностью. Основные преимущества бассейнов из бетона:",
-                    imageUrl : require('@/assets/images/concretePool.png')
+                    imageUrl : require('@/assets/images/concretePool.png'),
+                    listItems: [
+                        'Любые размеры и формы',
+                        'Долгий срок службы',
+                        'Установка дополнительного оборудования'
+                    ]
                 },
             ]
         }
--- components/poolTypes/poolTypesItem.vue
+++ components/poolTypes/poolTypesItem.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="px-6 py-6 flex flex-col justify-between items-center">
+    <div class="px-6 py-6 flex flex-col justify-between items-start mx-auto md:max-w-md sm:max-w-md">
         <div class="flex flex-row items-center">
-            <h3 class="text-xl uppercase ">
+            <h3 class="text-xl uppercase w-1/2">
                 <!-- <img class="h-5 w-5" src="@/assets/icons/check-mark.svg" alt="check-mark"> -->
                 {{title}}
             </h3>
         </div>
-        
-            <img class="w-3/4 h-3/5" :src="imageUrl" alt="image">
-        <p class="text-center">{{paragraph}}</p>
+        <div class="w-3/4 h-3/5 flex flex-row justify-start items-center p-5">
+            <img :src="imageUrl" alt="image">
+        </div>
+        <p>{{paragraph}}</p>
+        <ul class="list-none mt-3">
+            <li v-for="val in listItems" :key="val" class="flex flex-row items-center font-bold lg:text-sm xl:text-sm">
+                <img class="w-4 h-4 m-0 mr-2" src="@/assets/icons/check.svg" alt="check">
+                {{val}}
+            </li>
+        </ul>
+        <div>
+            <button></button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props : ['title','paragraph','imageUrl']
+    props : ['title','paragraph','imageUrl','listItems']
 }
 </script>
